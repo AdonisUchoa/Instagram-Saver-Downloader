e0b94ea4b6b8217d11745b3d1d1b1b525f3047ce
AdonisUchoa <adonisucho@gmail.com>
2022-10-24 15:58:53 -0300

    alteração nome

---
+++ app/src/main/java/com/app/Adonissaveinsta/util/YouApplication.java
@@ -0,0 +1,12 @@
+package com.app.Adonissaveinsta.util;
+
+import android.app.Application;
+
+public class YouApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
--- app/src/main/java/com/app/saveinstame/interfaces/OnClick.java
+++
@@ -1,9 +0,0 @@
-package com.app.saveinstame.interfaces;
-
-import java.io.Serializable;
-
-public interface OnClick extends Serializable {
-
-    void show(int position, String type, String data);
-
-}
